b3deabcd6da5b37a941925535895bd1eaa90ebaf
janaki10raman <janaki10raman@gmail.com>
2026-03-23 17:46:47 +0530

    Modify smoothie API response display
    Update the response handling for smoothie API call to display JSON data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered,'+name_on_order+'!', icon = "✅")
 #new session to display nutrition value
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+st.text(smoothiefroot_response.json())
