5ea638729ebf357ef0a6b8e79ef69ef1ce1c6f65
janaki10raman <janaki10raman@gmail.com>
2026-03-23 17:40:54 +0530

    Reintroduce nutrition value display with API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,11 +38,10 @@ if ingredients_list:
     # st.write(my_insert_stmt)
     # st.stop()
     time_to_insert = st.button("Submit Order")
-
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered,'+name_on_order+'!', icon = "✅")
 #new session to display nutrition value
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
 st.text(smoothiefroot_response)
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered,'+name_on_order+'!', icon = "✅")
